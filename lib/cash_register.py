d1f174124fed52ffe743e84330f902e076e61ef6
+ <+>
2025-11-19 10:00:15 +0300

    object orientation cash register lab

--- lib/cash_register.py
+++ lib/cash_register.py
@@ -1,4 +1,25 @@
 #!/usr/bin/env python3
 
 class CashRegister:
-  pass
+    def __init__(self, discount=0):
+        self.total = 0
+        self.discount = discount
+        self.items = []
+        self.last_transaction = 0
+
+    def add_item(self, title, price, quantity=1):
+        transaction_amount = price * quantity
+        self.total += transaction_amount
+        self.last_transaction = transaction_amount
+        for _ in range(quantity):
+            self.items.append(title)
+
+    def apply_discount(self):
+        if self.discount > 0:
+            self.total = int(self.total * (100 - self.discount) / 100)
+            print(f"After the discount, the total comes to ${self.total}.")
+        else:
+            print("There is no discount to apply.")
+
+    def void_last_transaction(self):
+        self.total -= self.last_transaction
